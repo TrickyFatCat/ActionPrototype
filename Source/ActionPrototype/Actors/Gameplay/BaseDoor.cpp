845dfa8a17dd46f6c01ad7c7a61908471a42f4f0
Tricky Fat Cat <tricky.fat.cat@gmail.com>
2021-03-17 16:46:56 +0300

    Create BaseDoor class

---
+++ Source/ActionPrototype/Actors/Gameplay/BaseDoor.cpp
@@ -0,0 +1,192 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseDoor.h"
+
+
+ABaseDoor::ABaseDoor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABaseDoor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABaseDoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+bool ABaseDoor::OpenDoor()
+{
+	if (CurrentState == EDoorState::Locked || CurrentState == EDoorState::Disabled || CurrentState == EDoorState::Opened
+	)
+	{
+		return false;
+	}
+
+	if (bIsTransitionRevertible && CurrentState == EDoorState::Transition && TargetState == EDoorState::Closed)
+	{
+		RevertTransition();
+		return true;
+	}
+
+	StartTransition();
+	return true;
+}
+
+bool ABaseDoor::CloseDoor()
+{
+	if (CurrentState == EDoorState::Locked || CurrentState == EDoorState::Disabled || CurrentState == EDoorState::Closed
+	)
+	{
+		return false;
+	}
+
+	if (bIsTransitionRevertible && CurrentState == EDoorState::Transition && TargetState == EDoorState::Opened)
+	{
+		RevertTransition();
+		return true;
+	}
+
+	StartTransition();
+	return true;
+}
+
+bool ABaseDoor::LockDoor()
+{
+	if (CurrentState == EDoorState::Locked || CurrentState != EDoorState::Closed)
+	{
+		return false;
+	}
+
+	ChangeStateTo(EDoorState::Locked);
+	return true;
+}
+
+bool ABaseDoor::UnlockDoor()
+{
+	if (CurrentState != EDoorState::Locked)
+	{
+		return false;
+	}
+
+	CurrentState = EDoorState::Closed;
+	OnUnlocked();
+	return true;
+}
+
+bool ABaseDoor::DisableDoor()
+{
+	if (CurrentState != EDoorState::Closed || CurrentState != EDoorState::Opened)
+	{
+		return false;
+	}
+
+	ChangeStateTo(EDoorState::Disabled);
+	return true;
+}
+
+bool ABaseDoor::EnableDoor(const EDoorState NewState)
+{
+	if (CurrentState != EDoorState::Disabled)
+	{
+		return false;
+	}
+
+	CurrentState = NewState;
+	OnEnabled();
+	return true;
+}
+
+void ABaseDoor::ChangeStateTo(const EDoorState NewState)
+{
+	PreviousState = CurrentState;
+	CurrentState = NewState;
+
+	switch (CurrentState)
+	{
+		case EDoorState::Closed:
+			OnClosed();
+			OnDoorClosed.Broadcast();
+			break;
+		case EDoorState::Opened:
+			OnOpened();
+			OnDoorOpened.Broadcast();
+
+			if (CloseDelay > 0.f)
+			{
+				FTimerDelegate CloseDelayDelegate;
+				CloseDelayDelegate.BindUFunction(this, FName("ChangeStateTo"), EDoorState::Closed);
+				GetWorld()->GetTimerManager().SetTimer(CloseDelayHandle, CloseDelayDelegate, CloseDelay, false);
+			}
+			break;
+		case EDoorState::Locked:
+			OnLocked();
+			OnDoorLocked.Broadcast();
+			break;
+		case EDoorState::Transition:
+			OnTransitionStarted();
+			OnDoorTransitionStarted.Broadcast();
+			break;
+		case EDoorState::Disabled:
+			OnDisabled();
+			break;
+		default:
+			break;
+	}
+}
+
+void ABaseDoor::StartTransition()
+{
+	if (CurrentState == EDoorState::Closed)
+	{
+		TargetState = EDoorState::Opened;
+
+		if (GetWorld()->GetTimerManager().IsTimerActive(CloseDelayHandle))
+		{
+			GetWorld()->GetTimerManager().ClearTimer(CloseDelayHandle);
+		}
+	}
+	else
+	{
+		TargetState = EDoorState::Closed;
+	}
+
+	TransitionDurationDelegate.Unbind();
+	TransitionDurationDelegate.BindUFunction(this, FName("ChangeStateTo"), TargetState);
+	GetWorld()->GetTimerManager().SetTimer(
+										   TransitionDurationHandle,
+										   TransitionDurationDelegate,
+										   TransitionDuration,
+										   false
+										  );
+	ChangeStateTo(EDoorState::Transition);
+}
+
+void ABaseDoor::RevertTransition()
+{
+	if (TargetState == EDoorState::Closed)
+	{
+		TargetState = EDoorState::Opened;
+	}
+	else
+	{
+		TargetState = EDoorState::Closed;
+	}
+
+	TransitionDurationDelegate.Unbind();
+	TransitionDurationDelegate.BindUFunction(this, FName("ChangeStateTo"), TargetState);
+	const float NewTransitionDuration = GetWorld()->GetTimerManager().GetTimerElapsed(TransitionDurationHandle);
+	GetWorld()->GetTimerManager().ClearTimer(TransitionDurationHandle);
+	GetWorld()->GetTimerManager().SetTimer(
+										   TransitionDurationHandle,
+										   TransitionDurationDelegate,
+										   NewTransitionDuration,
+										   false
+										  );
+	OnTransitionReverted();
+	OnDoorTransitionReverted.Broadcast();
+}
---
+++ Source/ActionPrototype/Actors/Gameplay/BaseDoor.h
@@ -0,0 +1,112 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseDoor.generated.h"
+
+class UStaticMeshComponent;
+
+UENUM(BlueprintType)
+enum class EDoorState : uint8
+{
+	Closed UMETA(DisplayName="Closed"),
+	Opened UMETA(DisplayName="Opened"),
+	Locked UMETA(DisplayName="Locked"),
+	Transition UMETA(DisplayName="Transition"),
+	Disabled UMETA(DisplayName="Disabled")
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDoorClosed);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDoorOpened);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDoorLocked);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDoorTransitionStarted);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDoorTransitionReverted);
+
+UCLASS()
+class ACTIONPROTOTYPE_API ABaseDoor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABaseDoor();
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category="Door")
+	bool OpenDoor();
+	UFUNCTION(BlueprintCallable, Category="Door")
+	bool CloseDoor();
+	UFUNCTION(BlueprintCallable, Category="Door")
+	bool LockDoor();
+	UFUNCTION(BlueprintCallable, Category="Door")
+	bool UnlockDoor();
+	UFUNCTION(BlueprintCallable, Category="Door")
+	bool DisableDoor();
+	UFUNCTION(BlueprintCallable, Category="Door")
+	bool EnableDoor(const EDoorState NewState);
+
+	UPROPERTY(BlueprintAssignable, Category="Door|Delegates")
+	FOnDoorClosed OnDoorClosed;
+	UPROPERTY(BlueprintAssignable, Category="Door|Delegates")
+	FOnDoorOpened OnDoorOpened;
+	UPROPERTY(BlueprintAssignable, Category="Door|Delegates")
+	FOnDoorLocked OnDoorLocked;
+	UPROPERTY(BlueprintAssignable, Category="Door|Delegates")
+	FOnDoorTransitionStarted OnDoorTransitionStarted;
+	UPROPERTY(BlueprintAssignable, Category="Door|Delegates")
+	FOnDoorTransitionReverted OnDoorTransitionReverted;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Door")
+	void OnOpened();
+	UFUNCTION(BlueprintImplementableEvent, Category="Door")
+	void OnClosed();
+	UFUNCTION(BlueprintImplementableEvent, Category="Door")
+	void OnLocked();
+	UFUNCTION(BlueprintImplementableEvent, Category="Door")
+	void OnUnlocked();
+	UFUNCTION(BlueprintImplementableEvent, Category="Door")
+	void OnDisabled();
+	UFUNCTION(BlueprintImplementableEvent, Category="Door")
+	void OnEnabled();
+	UFUNCTION(BlueprintImplementableEvent, Category="Door")
+	void OnTransitionStarted();
+	UFUNCTION(BlueprintImplementableEvent, Category="Door")
+	void OnTransitionReverted();
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Door", meta=(AllowPrivateAccess = "true"))
+	EDoorState InitialState{EDoorState::Closed};
+	UPROPERTY(BlueprintReadOnly, Category="Door", meta=(AllowPrivateAccess = "true"))
+	EDoorState CurrentState{InitialState};
+	UPROPERTY(BlueprintReadOnly, Category="Door", meta=(AllowPrivateAccess = "true"))
+	EDoorState TargetState;
+	UPROPERTY(BlueprintReadOnly, Category="Door", meta=(AllowPrivateAccess = "true"))
+	EDoorState PreviousState;
+	UFUNCTION()
+	void ChangeStateTo(const EDoorState NewState);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Door", meta=(AllowPrivateAccess="true"))
+	float TransitionDuration{0.25f};
+	UPROPERTY(BlueprintReadOnly, Category="Door|Time Handles", meta=(AllowPrivateAccess="true"))
+	FTimerHandle TransitionDurationHandle;
+	FTimerDelegate TransitionDurationDelegate;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Door", meta=(AllowPrivateAccess = "true"))
+	bool bIsTransitionRevertible{false};
+	UFUNCTION()
+	void StartTransition();
+	UFUNCTION()
+	void RevertTransition();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Door", meta=(AllowPrivateAccess = "true", ClampMin = "1.0"))
+	float CloseDelay{5.f};
+	UPROPERTY(BlueprintReadOnly, Category="Door|Time Handles", meta=(AllowPrivateAccess="true"))
+	FTimerHandle CloseDelayHandle;
+};
