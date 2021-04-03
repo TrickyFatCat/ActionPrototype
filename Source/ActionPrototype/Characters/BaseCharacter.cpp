d55f2a8a0f85c5e9fdf23399da794a22d80cc38a
Tricky Fat Cat <tricky.fat.cat@gmail.com>
2021-04-03 19:27:37 +0300

    Implement simple damagea and heal system to a BaseCharacter

--- Source/ActionPrototype/Characters/BaseCharacter.cpp
+++ Source/ActionPrototype/Characters/BaseCharacter.cpp
@@ -13,14 +13,81 @@ ABaseCharacter::ABaseCharacter()
 
 void ABaseCharacter::BeginPlay()
 {
+	OnTakeAnyDamage.AddDynamic(this, &ABaseCharacter::DecreaseCurrentHealth);
+	HealthComponent->OnCurrentValueIncreased.AddDynamic(this, &ABaseCharacter::BroadcastCurrentHealthIncreased);
+	HealthComponent->OnCurrentValueDecreased.AddDynamic(this, &ABaseCharacter::BroadcastCurrentHealthDecreased);
 	Super::BeginPlay();
+}
+
+void ABaseCharacter::BroadcastCurrentHealthIncreased(const float Amount, const float CurrentHealth)
+{
+	OnCurrentHealthIncreased.Broadcast(Amount, CurrentHealth);
+}
+
+void ABaseCharacter::BroadcastCurrentHealthDecreased(const float Amount, const float CurrentHealth)
+{
+	OnCurrentHealthDecreased.Broadcast(Amount, CurrentHealth);
 }
 
 void ABaseCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+float ABaseCharacter::GetCurrentHealth() const
+{
+	return HealthComponent->GetCurrentValue();
+}
 
+float ABaseCharacter::GetMaxHealth() const
+{
+	return HealthComponent->GetMaxValue();
+}
 
+float ABaseCharacter::GetNormalisedHealth() const
+{
+	return HealthComponent->GetNormalizedValue();
+}
+
+void ABaseCharacter::IncreaseCurrentHealth(const float Heal, const bool bClampToMax)
+{
+	HealthComponent->IncreaseValue(Heal, bClampToMax);
+}
+
+void ABaseCharacter::DecreaseCurrentHealth(
+	AActor* DamagedActor,
+	float Damage,
+	const UDamageType* DamageType,
+	AController* InstigatedBy,
+	AActor* DamageCauser)
+{
+	if (bIsInvulnerable && GetCurrentHealth() <= 0)
+	{
+		return;
+	}
+
+	HealthComponent->DecreaseValue(Damage);
+	const float CurrentHealth = GetCurrentHealth();
+
+	if (CurrentHealth <= 0.f)
+	{
+		OnZeroHealth();
+	}
+}
+
+void ABaseCharacter::IncreaseMaxHealth(const float Amount, const bool bClampCurrentValue)
+{
+	HealthComponent->IncreaseMaxValue(Amount, bClampCurrentValue);
+	OnMaxHealthIncreased.Broadcast(Amount, GetMaxHealth());
+}
+
+void ABaseCharacter::DecreaseMaxHealth(const float Amount, const bool bClampCurrentValue)
+{
+	if (GetMaxHealth() <= 0.f)
+	{
+		return;
+	}
+
+	HealthComponent->DecreaseMaxValue(Amount, bClampCurrentValue);
+	OnMaxHealthDecreased.Broadcast(Amount, GetMaxHealth());
+}
--- Source/ActionPrototype/Characters/BaseCharacter.h
+++ Source/ActionPrototype/Characters/BaseCharacter.h
@@ -9,6 +9,14 @@
 
 class UBaseResourceComponent;
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnCurrentHealthIncreased, float, Amount, float, NewValue);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnCurrentHealthDecreased, float, Amount, float, NewValue);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnMaxHealthIncreased, float, Amount, float, NewMaxHealth);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnMaxHealthDecreased, float, Amount, float, NewMaxHealth);
+
 UCLASS(Abstract)
 class ACTIONPROTOTYPE_API ABaseCharacter : public ACharacter
 {
@@ -17,11 +25,50 @@ class ACTIONPROTOTYPE_API ABaseCharacter : public ACharacter
 public:
 	ABaseCharacter();
 	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Character Health")
+	bool bIsInvulnerable{false};
+
+	UFUNCTION(BlueprintPure, Category="Character Health")
+	float GetCurrentHealth() const;
+	UFUNCTION(BlueprintPure, Category="Character Health")
+	float GetMaxHealth() const;
+	UFUNCTION(BlueprintPure, Category="Character Health")
+	float GetNormalisedHealth() const;
+	UFUNCTION(BlueprintCallable, Category="Character Health")
+	void IncreaseCurrentHealth(const float Heal, const bool bClampToMax = true);
+	UFUNCTION(BlueprintCallable, Category="Character Health")
+	void DecreaseCurrentHealth(
+		AActor* DamagedActor,
+		float Damage,
+		const UDamageType* DamageType,
+		AController* InstigatedBy,
+		AActor* DamageCauser);
+	UFUNCTION(BlueprintCallable, Category="Character Health")
+	void IncreaseMaxHealth(const float Amount, const bool bClampCurrentValue = true);
+	UFUNCTION(BlueprintCallable, Category="Character Health")
+	void DecreaseMaxHealth(const float Amount, const bool bClampCurrentValue = true);
+
+	UPROPERTY(BlueprintAssignable, Category="Character Health")
+	FOnCurrentHealthIncreased OnCurrentHealthIncreased;
+	UPROPERTY(BlueprintAssignable, Category="Character Health")
+	FOnCurrentHealthDecreased OnCurrentHealthDecreased;
+	UPROPERTY(BlueprintAssignable, Category="Character Health")
+	FOnMaxHealthIncreased OnMaxHealthIncreased;
+	UPROPERTY(BlueprintAssignable, Category="Character Health")
+	FOnMaxHealthDecreased OnMaxHealthDecreased;
 
 protected:
 	virtual void BeginPlay() override;
 	
+	UFUNCTION(BlueprintImplementableEvent, Category="Character Health")
+	void OnZeroHealth();
+
 private:
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess="true"))
 	UBaseResourceComponent* HealthComponent{nullptr};
+	
+	UFUNCTION()
+	void BroadcastCurrentHealthIncreased(const float Amount, const float CurrentHealth);
+	UFUNCTION()
+	void BroadcastCurrentHealthDecreased(const float Amount, const float CurrentHealth);
 };
