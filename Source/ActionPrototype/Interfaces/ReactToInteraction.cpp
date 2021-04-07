33843e9101aaf045cdf147b6ee46707d59f054ac
Tricky Fat Cat <tricky.fat.cat@gmail.com>
2021-04-07 19:53:16 +0300

    Create new interface for interactable objects

---
+++ Source/ActionPrototype/Interfaces/ReactToInteraction.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ReactToInteraction.h"
+
+
+// Add default functionality here for any IReactToInteraction functions that are not pure virtual.
---
+++ Source/ActionPrototype/Interfaces/ReactToInteraction.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "UObject/Interface.h"
+#include "ReactToInteraction.generated.h"
+
+class APlayerCharacter;
+
+// This class does not need to be modified.
+UINTERFACE()
+class UReactToInteraction : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class ACTIONPROTOTYPE_API IReactToInteraction
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="Interaction Interface")
+	void ProcessInteraction(const APlayerCharacter* PlayerCharacter);
+	TScriptInterface<bool> bIsInteractable{};
+};
